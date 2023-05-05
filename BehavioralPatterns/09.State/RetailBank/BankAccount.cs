4fb3c9298b7a16271305825339a8e5ab6b6f2810
hthumbaraguddi <harsha.thumbaraguddi@gmail.com>
2023-05-05 12:43:58 +0530

    State Design Pattern

---
+++ BehavioralPatterns/09.State/RetailBank/BankAccount.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailBank
+{
+    public class BankAccount
+    {
+        public AccountState AccountState { get; set; }
+
+        public decimal Balance { get { return AccountState.Balance; } }
+        public BankAccount(decimal initialDeposite) 
+        {            
+            AccountState = new RegularAccountState(this, initialDeposite);
+        }
+
+        public void Deposite(decimal money)
+        {
+            AccountState.Deposite(money);
+        }
+
+        public void WithDraw(decimal money)
+        {
+            AccountState.Withdraw(money);
+        }
+    }
+
+    public abstract class AccountState
+    {
+        public decimal Balance { get; protected set; }
+        public abstract void Deposite(decimal amount);
+        public abstract void Withdraw(decimal amount);
+
+    }
+
+    public class RegularAccountState : AccountState
+    {
+        private BankAccount bankAccount;
+       
+
+        public RegularAccountState(BankAccount bankAccount, decimal balance) 
+        {
+            this.bankAccount = bankAccount;
+            this.Balance = balance;
+        }
+        public override void Deposite(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            
+            this.Balance -= amount;
+
+            if(this.Balance <= 0) 
+            {
+                this.bankAccount.AccountState = new OverDrawnAccountState(bankAccount, Balance);
+                Console.WriteLine("Account stateis OverDrawn");
+            }
+        }
+        
+    }
+
+    internal class OverDrawnAccountState : AccountState
+    {
+        private BankAccount bankAccount;
+       
+
+        public OverDrawnAccountState(BankAccount bankAccount, decimal balance)
+        {
+            this.bankAccount = bankAccount;
+            this.Balance = balance;
+        }
+
+        public override void Deposite(decimal amount)
+        {
+            this.Balance += amount;
+            if(Balance > 0)
+            {
+                this.bankAccount.AccountState = new RegularAccountState(bankAccount, Balance);
+                Console.WriteLine("Welcome Bank, Account state is Regular now");
+            }
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            Console.WriteLine($"Balance is less than 0 so no more money for you");
+        }
+    }
+}
---
+++ BehavioralPatterns/09.State/RetailBank/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RetailBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(500);
+
+            account.Deposite(100);
+            account.WithDraw(601);
+            account.Deposite(100);
+            Console.ReadLine();
+        }
+    }
+}
